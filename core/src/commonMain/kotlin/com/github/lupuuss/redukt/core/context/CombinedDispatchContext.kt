6cb08d409bbba3f69e3caefa80e0ff5df5676ef4
lupuuss <damian.baczynski@gmail.com>
2022-03-31 22:06:37 +0200

    Add not empty list requirement to CombinedDispatchContext

--- core/src/commonMain/kotlin/com/github/lupuuss/redukt/core/context/CombinedDispatchContext.kt
+++ core/src/commonMain/kotlin/com/github/lupuuss/redukt/core/context/CombinedDispatchContext.kt
@@ -4,6 +4,10 @@ internal class CombinedDispatchContext(
     private val elements: List<DispatchContext.Element>
 ) : DispatchContext {
 
+    init {
+        require(elements.isNotEmpty()) { "Combined context cannot be empty!" }
+    }
+
     @Suppress("UNCHECKED_CAST")
     override fun <T : DispatchContext.Element> find(key: DispatchContext.Key<T>): T? = elements.find { it.key == key } as? T
 
