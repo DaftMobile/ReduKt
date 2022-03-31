fb599e1c395a6b9ef9223a1d4acc884080a5766c
lupuuss <damian.baczynski@gmail.com>
2022-03-31 16:57:28 +0200

    Add StoreImpl.kt

---
+++ core/src/commonMain/kotlin/com/github/lupuuss/redukt/core/store/StoreImpl.kt
@@ -0,0 +1,29 @@
+package com.github.lupuuss.redukt.core.store
+
+import com.github.lupuuss.redukt.core.Action
+import com.github.lupuuss.redukt.core.Reducer
+import com.github.lupuuss.redukt.core.context.DispatchContext
+import com.github.lupuuss.redukt.core.context.EmptyDispatchContext
+import com.github.lupuuss.redukt.core.middleware.Middleware
+import com.github.lupuuss.redukt.core.scope.CoreDispatchScope
+import kotlinx.coroutines.flow.MutableStateFlow
+
+internal class StoreImpl<State>(
+    initialState: State,
+    private val reducer: Reducer<State>,
+    private val middlewares: List<Middleware<State>> = emptyList(),
+    context: DispatchContext = EmptyDispatchContext
+) : Store<State> {
+    override val state = MutableStateFlow(initialState)
+
+    private val scope = CoreDispatchScope(context, this::dispatch, this.state::value)
+
+    override fun dispatch(action: Action) {
+        var status = Middleware.Status.Passed
+        for (middleware in middlewares) {
+            status = middleware.run { scope.process(action) }
+            if (status == Middleware.Status.Consumed) break
+        }
+        if (status == Middleware.Status.Passed) state.value = reducer(state.value, action)
+    }
+}
