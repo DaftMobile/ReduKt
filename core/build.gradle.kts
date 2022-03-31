e1861408c6dd1490993b578667e200c5df7bf179
lupuuss <damian.baczynski@gmail.com>
2022-03-31 16:53:47 +0200

    Init core project

---
+++ core/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("org.kodein.mock.mockmp") version "1.4.0"
+}
+
+setupReduKtPackage()
+
+mockmp {
+    usesHelper = true
+}
+
+kotlin {
+    allSupportedTargets()
+    sourceSets {
+        commonOptIns()
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${ReduKt.vCoroutines}")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${ReduKt.vCoroutines}")
+            }
+        }
+    }
+}
