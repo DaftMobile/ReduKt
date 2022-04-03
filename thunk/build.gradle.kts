eccbee07bf676abc30c84a8bd548efc308f73067
lupuuss <damian.baczynski@gmail.com>
2022-04-03 12:39:04 +0200

    Add thunk

---
+++ thunk/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.kodein.mock.mockmp") version "1.4.0"
+}
+
+setupReduKtPackage()
+
+mockmp {
+    usesHelper = true
+}
+
+kotlin {
+    allSupportedTargets()
+    sourceSets {
+        commonOptIns()
+        val commonMain by getting {
+            dependencies {
+                api(project(":core"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${ReduKt.vCoroutines}")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${ReduKt.vCoroutines}")
+            }
+        }
+    }
+}
---
+++ thunk/src/commonMain/kotlin/com/github/lupuuss/redukt/thunk/Thunk.kt
@@ -0,0 +1,33 @@
+package com.github.lupuuss.redukt.thunk
+
+import com.github.lupuuss.redukt.core.Action
+import com.github.lupuuss.redukt.core.scope.DispatchScope
+import kotlinx.coroutines.CoroutineStart
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+sealed interface Thunk : Action {
+
+    interface Executable<State> : Thunk {
+        fun DispatchScope<State>.execute()
+    }
+
+    abstract class Suspendable<State>(
+        val coroutineContext: CoroutineContext = EmptyCoroutineContext,
+        val coroutineStart: CoroutineStart = CoroutineStart.DEFAULT,
+    ) : Thunk {
+        abstract suspend fun DispatchScope<State>.executeSuspendable()
+    }
+
+    open class Execute<State>(private val block: DispatchScope<State>.() -> Unit) : Executable<State> {
+        override fun DispatchScope<State>.execute() = block()
+    }
+
+    open class Suspend<State>(
+        private val block: suspend  DispatchScope<State>.() -> Unit,
+        coroutineContext: CoroutineContext = EmptyCoroutineContext,
+        coroutineStart: CoroutineStart = CoroutineStart.DEFAULT,
+    ) : Suspendable<State>(coroutineContext, coroutineStart) {
+        override suspend fun DispatchScope<State>.executeSuspendable() = block()
+    }
+}
