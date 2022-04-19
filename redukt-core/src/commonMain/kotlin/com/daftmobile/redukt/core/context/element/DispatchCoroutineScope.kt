699b4f8b90e614495552571b81c788e2011d5482
lupuuss <damian.baczynski@gmail.com>
2022-04-19 22:42:21 +0200

    Change receiver of coroutineScope from DispatchScope<*> to ActionDispatcher.kt

--- redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/context/element/DispatchCoroutineScope.kt
+++ redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/context/element/DispatchCoroutineScope.kt
@@ -1,7 +1,7 @@
 package com.daftmobile.redukt.core.context.element
 
+import com.daftmobile.redukt.core.ActionDispatcher
 import com.daftmobile.redukt.core.context.DispatchContext
-import com.daftmobile.redukt.core.scope.DispatchScope
 import kotlinx.coroutines.CoroutineScope
 
 class DispatchCoroutineScope(scope: CoroutineScope) : DispatchContext.Element, CoroutineScope by scope {
@@ -10,4 +10,4 @@ class DispatchCoroutineScope(scope: CoroutineScope) : DispatchContext.Element, C
     companion object Key : DispatchContext.Key<DispatchCoroutineScope>
 }
 
-val DispatchScope<*>.coroutineScope get() = dispatchContext[DispatchCoroutineScope]
+inline val ActionDispatcher.coroutineScope get() = dispatchContext[DispatchCoroutineScope]
