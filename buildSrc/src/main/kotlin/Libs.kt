6865c71952084501c7023e310a89c6dff37f95cd
lupuuss <damian.baczynski@gmail.com>
2022-04-20 01:43:47 +0200

    Set coroutines to 1.6.1 (no native-mt)

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,7 +1,8 @@
+@Suppress("ClassName")
 object Libs {
     object kotlinx {
         object coroutines {
-            const val version = "1.6.0-native-mt"
+            const val version = "1.6.1"
             const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
             const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
         }
