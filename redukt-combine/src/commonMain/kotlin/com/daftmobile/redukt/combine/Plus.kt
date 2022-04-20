0bfc51d8afcc6e22cccbd0e74afacaf7ee971174
lupuuss <damian.baczynski@gmail.com>
2022-04-21 00:29:44 +0200

    Add Plus.kt

---
+++ redukt-combine/src/commonMain/kotlin/com/daftmobile/redukt/combine/Plus.kt
@@ -0,0 +1,13 @@
+package com.daftmobile.redukt.combine
+
+import com.daftmobile.redukt.core.Action
+import com.daftmobile.redukt.core.scope.DispatchScope
+import com.daftmobile.redukt.thunk.Thunk
+
+data class PlusCombined(val actions: List<Action>) : Thunk.Executable<Nothing> {
+    override fun DispatchScope<Nothing>.execute() = actions.forEach(::dispatch)
+}
+
+operator fun Action.plus(action: Action) = PlusCombined(this.unwrapped() + action.unwrapped())
+
+private fun Action.unwrapped() = if (this is PlusCombined) actions else listOf(this)
