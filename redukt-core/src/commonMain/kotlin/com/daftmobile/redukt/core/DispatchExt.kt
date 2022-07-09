dc2449c4064b3f882ce29639e5add18b8ea3907b
lupuuss <damian.baczynski@gmail.com>
2022-07-09 17:36:42 +0200

    Make StoreImpl dispatch always in DispatchCoroutineScope context

--- redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/DispatchExt.kt
+++ redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/DispatchExt.kt
@@ -10,14 +10,4 @@ import kotlinx.coroutines.launch
  */
 fun ActionDispatcher.launchDispatch(action: Action) = dispatchContext[DispatchCoroutineScope].launch { dispatch(action) }
 
-/**
- * Just like [ActionDispatcher.launchDispatch], executes [ActionDispatcher.dispatch] in a new coroutine using
- * [DispatchCoroutineScope] from [ActionDispatcher.dispatchContext]. Additionally, this extension joins created coroutine.
- * Therefore, the outcome of dispatched [action] could be expected after this function returns.
- * @param action to be dispatched
- */
-suspend fun ActionDispatcher.syncDispatch(action: Action) = dispatchContext[DispatchCoroutineScope]
-    .launch { dispatch(action) }
-    .join()
-
 suspend fun ActionDispatcher.dispatchIfPresent(action: Action?) = action?.let { dispatch(it) }
--- redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/store/StoreImpl.kt
+++ redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/store/StoreImpl.kt
@@ -3,28 +3,33 @@ package com.daftmobile.redukt.core.store
 import com.daftmobile.redukt.core.Action
 import com.daftmobile.redukt.core.Reducer
 import com.daftmobile.redukt.core.context.DispatchContext
-import com.daftmobile.redukt.core.context.EmptyDispatchContext
+import com.daftmobile.redukt.core.context.element.DispatchCoroutineScope
+import com.daftmobile.redukt.core.context.element.coroutineScope
 import com.daftmobile.redukt.core.middleware.Middleware
 import com.daftmobile.redukt.core.middleware.MiddlewareStatus
 import com.daftmobile.redukt.core.scope.CoreDispatchScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.withContext
 
 internal class StoreImpl<State>(
     initialState: State,
     private val reducer: Reducer<State>,
     private val middlewares: List<Middleware<State>> = emptyList(),
-    override val dispatchContext: DispatchContext = EmptyDispatchContext
+    override val dispatchContext: DispatchContext = DispatchCoroutineScope(MainScope())
 ) : Store<State> {
     override val state = MutableStateFlow(initialState)
 
     private val scope = CoreDispatchScope(dispatchContext, this::dispatch, this.state::value)
 
     override suspend fun dispatch(action: Action) {
-        var status: MiddlewareStatus = MiddlewareStatus.Next(action)
-        for (middleware in middlewares) {
-            status = middleware(scope, action)
-            if (status is MiddlewareStatus.Consumed) break
+        withContext(coroutineScope.coroutineContext) {
+            var status: MiddlewareStatus = MiddlewareStatus.Next(action)
+            for (middleware in middlewares) {
+                status = middleware(scope, action)
+                if (status is MiddlewareStatus.Consumed) break
+            }
+            if (status is MiddlewareStatus.Next) state.value = reducer(state.value, action)
         }
-        if (status is MiddlewareStatus.Next) state.value = reducer(state.value, action)
     }
 }
