d146bd31a9f1c4db40eff4197522bbd89cac789e
lupuuss <damian.baczynski@gmail.com>
2022-07-15 23:10:41 +0200

    Add MainScope default to DispatchCoroutineScope.kt

--- redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/context/element/DispatchCoroutineScope.kt
+++ redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/context/element/DispatchCoroutineScope.kt
@@ -3,8 +3,9 @@ package com.daftmobile.redukt.core.context.element
 import com.daftmobile.redukt.core.ActionDispatcher
 import com.daftmobile.redukt.core.context.DispatchContext
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 
-class DispatchCoroutineScope(scope: CoroutineScope) : DispatchContext.Element, CoroutineScope by scope {
+class DispatchCoroutineScope(scope: CoroutineScope = MainScope()) : DispatchContext.Element, CoroutineScope by scope {
     override val key get() = Key
 
     companion object Key : DispatchContext.Key<DispatchCoroutineScope>
