60227d057b6b87869bfba4467c160b8be6650a0f
lupuuss <damian.baczynski@gmail.com>
2022-12-15 22:12:25 +0100

    Make ThunkTester.kt strict by default

--- redukt-test-thunk/src/commonMain/kotlin/dev/redukt/test/thunk/ThunkTester.kt
+++ redukt-test-thunk/src/commonMain/kotlin/dev/redukt/test/thunk/ThunkTester.kt
@@ -3,6 +3,7 @@ package dev.redukt.test.thunk
 import dev.redukt.core.closure.DispatchClosure
 import dev.redukt.core.closure.EmptyDispatchClosure
 import dev.redukt.test.assertions.ActionsAssertScope
+import dev.redukt.test.assertions.expectNoMoreActions
 import dev.redukt.test.tools.ImmutableLocalClosure
 import dev.redukt.test.tools.MockForegroundJobRegistry
 import dev.redukt.test.tools.SpyingDispatchScope
@@ -12,6 +13,7 @@ import dev.redukt.thunk.ThunkAction
 public fun <State> ThunkAction<State>.testExecute(
     state: State,
     closure: DispatchClosure = EmptyDispatchClosure,
+    strict: Boolean = true,
     testBlock: ActionsAssertScope.() -> Unit
 ) {
     val resultingClosure = MockForegroundJobRegistry() + closure
@@ -21,12 +23,14 @@ public fun <State> ThunkAction<State>.testExecute(
     ).run {
         execute()
         testBlock()
+        if (strict) expectNoMoreActions()
     }
 }
 
 public suspend fun <State> CoThunkAction<State>.testExecute(
     state: State,
     closure: DispatchClosure = EmptyDispatchClosure,
+    strict: Boolean = true,
     testBlock: suspend ActionsAssertScope.() -> Unit
 ) {
     val initialClosure = MockForegroundJobRegistry() + closure
@@ -36,5 +40,6 @@ public suspend fun <State> CoThunkAction<State>.testExecute(
     ).run {
         execute()
         testBlock()
+        if (strict) expectNoMoreActions()
     }
 }
