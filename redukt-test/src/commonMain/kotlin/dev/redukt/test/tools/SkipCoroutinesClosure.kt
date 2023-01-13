5d69c78619ccc1169db12434cab5a6476aea81f3
lupuuss <damian.baczynski@gmail.com>
2023-01-13 21:33:35 +0100

    Remove DelicateReduKtApi from SkipCoroutinesClosure

--- redukt-test/src/commonMain/kotlin/dev/redukt/test/tools/SkipCoroutinesClosure.kt
+++ redukt-test/src/commonMain/kotlin/dev/redukt/test/tools/SkipCoroutinesClosure.kt
@@ -1,11 +1,9 @@
 package dev.redukt.test.tools
 
-import dev.redukt.core.DelicateReduKtApi
 import dev.redukt.core.closure.DispatchClosure
 
 /**
  * Creates a [DispatchClosure] that mocks foreground coroutines mechanism. Every [dev.redukt.core.coroutines.dispatchJob]
  * is executed with no failure and returns completed job.
  */
-@DelicateReduKtApi
 public fun SkipCoroutinesClosure(): DispatchClosure = TestLocalClosureContainer() + TestForegroundJobRegistry()
