bcc092973aabcaf1e94ff1f40c1241e6dea36ec6
lupuuss <damian.baczynski@gmail.com>
2023-01-29 16:21:13 +0100

    Fix SelectStateFlow to properly handle multiple subscribers and nulls

--- redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/store/NewSelectStateFlowProvider.kt
+++ redukt-core/src/commonMain/kotlin/com/daftmobile/redukt/core/store/NewSelectStateFlowProvider.kt
@@ -13,37 +13,45 @@ internal class NewSelectStateFlowProvider : SelectStateFlowProvider {
     ): StateFlow<Selected> = SelectStateFlow(state, selector)
 }
 
+@Suppress("UNCHECKED_CAST")
 internal class SelectStateFlow<State, Selection>(
     private val flow: StateFlow<State>,
     private val selector: Selector<State, Selection>
 ) : StateFlow<Selection>, SynchronizedObject() {
 
-    private var lastProcessedState: State? by atomic(null)
-
-    private var lastMappedValue: Selection? by atomic(null)
+    private var lastProcessedState: Any? by atomic(NULL)
+    private var lastMappedValue: Any? by atomic(NULL)
 
     override val replayCache: List<Selection> get() = listOf(value)
 
-    override val value: Selection get() = (invalidate() ?: lastMappedValue)!!
+    override val value: Selection get() = invalidate()
 
     override suspend fun collect(collector: FlowCollector<Selection>): Nothing {
-        lastMappedValue?.let { collector.emit(it) }
+        val lastMappedValue = this.lastMappedValue
+        if (lastMappedValue !== NULL) collector.emit(lastMappedValue as Selection)
+        var previousValue: Any? = lastMappedValue
         flow.collect { value ->
-            val previousValue = lastMappedValue
-            invalidate(value)
-                ?.takeIf { previousValue == null || !selector.isSelectionEqual(previousValue, it) }
-                ?.let { collector.emit(it) }
+            val prev = previousValue
+            val mappedValue = invalidate(value)
+            if (prev === NULL || !selector.isSelectionEqual(prev as Selection, mappedValue)) {
+                previousValue = mappedValue
+                collector.emit(mappedValue)
+            }
         }
     }
 
-    private fun invalidate(currentState: State = flow.value): Selection? = synchronized(this) {
+    private fun invalidate(currentState: State = flow.value): Selection = synchronized(this) {
         val previousState = lastProcessedState
         lastProcessedState = currentState
         return when {
-            previousState == null || !selector.isStateEqual(previousState, currentState) -> {
+            previousState === NULL || !selector.isStateEqual(previousState as State, currentState) -> {
                 selector.select(currentState).also { lastMappedValue = it }
             }
-            else -> null
+
+            else -> lastMappedValue as Selection
         }
     }
+
+    // we must not refer to real null because State and Selection might be nullable
+    private object NULL
 }
--- redukt-core/src/commonTest/kotlin/com/daftmobile/redukt/core/store/SelectTest.kt
+++ redukt-core/src/commonTest/kotlin/com/daftmobile/redukt/core/store/SelectTest.kt
@@ -4,8 +4,11 @@ import app.cash.turbine.test
 import com.daftmobile.redukt.core.KnownAction
 import com.daftmobile.redukt.core.MockSelector
 import com.daftmobile.redukt.core.closure.EmptyDispatchClosure
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
@@ -50,6 +53,43 @@ class SelectTest {
     }
 
     @Test
+    fun shouldReturnStateFlowThatProvidesMappedStateUpdatesForMultipleSubscribers() = runTest {
+        val subState = store.select { it.coerceAtMost(3).toString() }
+        val results = mutableListOf<List<String>>()
+        repeat(10) {
+            launch {
+                results.add(subState.take(3).toList())
+            }
+        }
+        yield()
+        store.dispatch(KnownAction.A)
+        yield()
+        store.dispatch(KnownAction.A)
+        yield()
+        store.dispatch(KnownAction.A)
+        yield()
+        results shouldHaveSize 10
+        results.forEach {
+            it shouldBe listOf("1", "2", "3")
+        }
+    }
+
+    @Test
+    fun shouldEmitValuesProperlyOnRepeatedCollect() = runTest {
+        val subState = store.select { it.toString() }
+        subState.test {
+            store.dispatch(KnownAction.A)
+            awaitItem() shouldBe "1"
+            awaitItem() shouldBe "2"
+        }
+        subState.test {
+            awaitItem() shouldBe "2"
+            store.dispatch(KnownAction.A)
+            awaitItem() shouldBe "3"
+        }
+    }
+
+    @Test
     fun shouldNotEmitSelectedStateIfNotChanged() = runTest {
         val subState = store.select { "3" }
         subState.test {
@@ -75,5 +115,43 @@ class SelectTest {
         val selector = MockSelector<Int, Int> { it * it }
         store.select(selector = selector::call)
         selector.callsCounter shouldBe 0
+    }
+
+    @Test
+    fun shouldProperlyDeliverInitialNull() = runTest {
+        val subState = store.select { it.takeIf { it > 2 } }
+        subState.test {
+            awaitItem() shouldBe null
+        }
+    }
+
+    @Test
+    fun shouldNotRepeatNull() = runTest {
+        val subState = store.select { it.takeIf { it > 2 } }
+        subState.test {
+            skipItems(1)
+            store.dispatch(KnownAction.A)
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun shouldProperlyHandleNullTransition() = runTest {
+        val subState = store.select { it.takeIf { it > 2 } }
+        subState.test {
+            skipItems(1)
+            store.dispatch(KnownAction.A)
+            store.dispatch(KnownAction.A)
+            awaitItem() shouldBe 3
+        }
+    }
+
+    @Test
+    fun shouldProperlyDeliverNullOnRepeatedCollect() = runTest {
+        val subState = store.select { it.takeIf { it > 2 } }
+        subState.test { skipItems(1) }
+        subState.test {
+            awaitItem() shouldBe null
+        }
     }
 }
