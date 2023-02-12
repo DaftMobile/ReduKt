de75e13645d403cc5a36e7dc3b49b2db3aba5d34
lupuuss <damian.baczynski@gmail.com>
2023-02-12 20:28:12 +0100

    Add KmmPreviewStore

--- redukt-kmm/src/commonMain/kotlin/com/daftmobile/redukt/kmm/KmmStore.kt
+++ redukt-kmm/src/commonMain/kotlin/com/daftmobile/redukt/kmm/KmmStore.kt
@@ -1,12 +1,14 @@
-@file:OptIn(ExperimentalObjCRefinement::class)
+@file:OptIn(ExperimentalObjCRefinement::class, ExperimentalObjCName::class)
 
 package com.daftmobile.redukt.kmm
 
 import com.daftmobile.redukt.core.Action
+import com.daftmobile.redukt.core.Reducer
 import com.daftmobile.redukt.core.coroutines.ForegroundJobAction
 import com.daftmobile.redukt.core.coroutines.coroutineScope
 import com.daftmobile.redukt.core.coroutines.dispatchJob
 import com.daftmobile.redukt.core.store.Store
+import com.daftmobile.redukt.core.store.previewStore
 import com.daftmobile.redukt.core.store.select.Selector
 import com.daftmobile.redukt.core.store.select.select
 import kotlinx.coroutines.flow.Flow
@@ -18,57 +20,57 @@ import kotlin.native.HiddenFromObjC
 import kotlin.native.ObjCName
 import kotlin.native.ShouldRefineInSwift
 
-@OptIn(ExperimentalObjCName::class)
+/**
+ * Simplified [Store] API to provide better interop with Objective-C/Swift.
+ */
 @ObjCName("ReduKtStore", exact = true)
-public abstract class KmmStore<State> {
+public abstract class KmmStore<State : Any> {
 
     @HiddenFromObjC
     protected abstract val store: Store<State>
 
-    public abstract val state: State
-
-    public abstract fun subscribe(onStateChange: (State) -> Unit): Disposable
+    public open fun subscribe(onStateChange: (State) -> Unit): Disposable = store.state.subscribe(onStateChange)
 
     @ShouldRefineInSwift
-    public abstract fun <Selected> subscribe(
-        selector: Selector<State, Selected>,
-        onStateChange: (Selected) -> Unit
-    ): Disposable
-
-    @ShouldRefineInSwift
-    public abstract fun <Selected> subscribe(
-        selector: (State) -> Selected,
-        onStateChange: (Selected) -> Unit
-    ): Disposable
-
-    public abstract fun dispatch(action: Action)
-
-    public abstract fun dispatchJob(action: ForegroundJobAction): Disposable
-}
-
-public fun <State : Any> Store<State>.toKmmStore(): KmmStore<State> = KmmStoreImpl(this)
-
-private class KmmStoreImpl<State : Any>(override val store: Store<State>) : KmmStore<State>() {
-
-    override val state: State get() = store.currentState
-
-    override fun subscribe(onStateChange: (State) -> Unit): Disposable = store.state.subscribe(onStateChange)
-
-    override fun <Selected> subscribe(
+    public open fun <Selected> subscribe(
         selector: Selector<State, Selected>,
         onStateChange: (Selected) -> Unit
     ): Disposable = store.select(selector).subscribe(onStateChange)
 
-    override fun <Selected> subscribe(
+    @ShouldRefineInSwift
+    public open fun <Selected> subscribe(
         selector: (State) -> Selected,
         onStateChange: (Selected) -> Unit
     ): Disposable = store.select(selector).subscribe(onStateChange)
 
-    override fun dispatch(action: Action): Unit = store.dispatch(action)
+    public open fun dispatch(action: Action): Unit = store.dispatch(action)
 
-    override fun dispatchJob(action: ForegroundJobAction): Disposable = Disposable(store.dispatchJob(action)::cancel)
+    public open fun dispatchJob(action: ForegroundJobAction): Disposable = Disposable(store.dispatchJob(action)::cancel)
 
     private fun <T> Flow<T>.subscribe(onStateChange: (T) -> Unit): Disposable = onEach(onStateChange)
         .launchIn(store.coroutineScope)
         .let { Disposable(it::cancel) }
 }
+
+/**
+ * [previewStore] equivalent that implements [KmmStore].
+ */
+@ObjCName("ReduKtPreviewStore", exact = true)
+public class KmmPreviewStore<State : Any>(
+    initialState: State,
+    reducer: Reducer<State>,
+) : KmmStore<State>() {
+
+    public constructor(initialState: State) : this(initialState, { state, _ -> state })
+
+    @HiddenFromObjC
+    override val store: Store<State> = previewStore(initialState = initialState, reducer = reducer)
+}
+
+/**
+ * Returns [KmmStore] that delegates to [this] store.
+ */
+@HiddenFromObjC
+public fun <State : Any> Store<State>.toKmmStore(): KmmStore<State> = KmmStoreWrapper(this)
+
+private class KmmStoreWrapper<State : Any>(override val store: Store<State>) : KmmStore<State>()
