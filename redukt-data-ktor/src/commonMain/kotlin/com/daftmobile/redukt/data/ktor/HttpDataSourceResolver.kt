90d41f793b44d41e8a38fc13df65ab2fc914645f
lupuuss <damian.baczynski@gmail.com>
2023-02-18 17:02:05 +0100

    Move HttpClient config to DSL

--- redukt-data-ktor/src/commonMain/kotlin/com/daftmobile/redukt/data/ktor/HttpDataSourceResolver.kt
+++ redukt-data-ktor/src/commonMain/kotlin/com/daftmobile/redukt/data/ktor/HttpDataSourceResolver.kt
@@ -4,38 +4,60 @@ import com.daftmobile.redukt.data.DataSource
 import com.daftmobile.redukt.data.DataSourceKey
 import com.daftmobile.redukt.data.PureDataSourceKey
 import com.daftmobile.redukt.data.resolver.DataSourceResolver
+import com.daftmobile.redukt.data.resolver.TypeSafeResolverConfigMarker
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 public fun HttpDataSourceResolver(
-    client: HttpClient = HttpClient(),
     config: HttpResolverConfigScope.() -> Unit
-): DataSourceResolver = HttpResolverConfigScopeImpl(client)
-    .apply(config)
-    .build()
+): DataSourceResolver = HttpResolverBuilder().apply(config).build()
 
 public interface HttpResolverConfigScope {
 
     public var dispatcher: CoroutineDispatcher
     public var defaultErrorMapper: HttpErrorMapper?
 
+    @TypeSafeResolverConfigMarker
+    public fun client(client: HttpClient)
+
+    @TypeSafeResolverConfigMarker
+    public fun client(block: HttpClientConfig<*>.() -> Unit)
+
+    @TypeSafeResolverConfigMarker
+    public fun client(engine: HttpClientEngine, block: HttpClientConfig<*>.() -> Unit)
+
+    @TypeSafeResolverConfigMarker
     public infix fun <Request, Response, T : DataSource<Request, Response>> PureDataSourceKey<T>.resolvedBy(
         provider: () -> HttpEndpoint<Request, *, Response>
     )
 
+    @TypeSafeResolverConfigMarker
     public infix fun <Request, Response, T : DataSource<Request, Response>> PureDataSourceKey<T>.resolvesTo(
         endpoint: HttpEndpoint<Request, *, Response>
     )
 }
 
-private class HttpResolverConfigScopeImpl(
-    private val client: HttpClient,
-) : HttpResolverConfigScope {
+internal class HttpResolverBuilder : HttpResolverConfigScope {
 
+    private var clientInstance: HttpClient? = null
     private val providers = mutableMapOf<DataSourceKey<*, *>, () -> HttpEndpoint<*, *, *>>()
+
     override var dispatcher: CoroutineDispatcher = Dispatchers.Default
     override var defaultErrorMapper: HttpErrorMapper? = null
+    override fun client(client: HttpClient) {
+        clientInstance = client
+    }
+
+    override fun client(block: HttpClientConfig<*>.() -> Unit) {
+        clientInstance = HttpClient(block = block)
+    }
+
+    override fun client(engine: HttpClientEngine, block: HttpClientConfig<*>.() -> Unit) {
+        clientInstance = HttpClient(engine, block)
+    }
 
     override fun <Request, Response, T : DataSource<Request, Response>> PureDataSourceKey<T>.resolvedBy(
         provider: () -> HttpEndpoint<Request, *, Response>
@@ -62,6 +84,7 @@ private class HttpResolverConfigScopeImpl(
     }
 
     fun build(): DataSourceResolver = DataSourceResolver {
+        val client = clientInstance ?: HttpClient()
         providers.forEach { (key, endpoint) ->
             key resolvesTo HttpDataSource(client, dispatcher, endpoint())
         }
