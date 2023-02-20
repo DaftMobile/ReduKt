6bcbccc8d563a1272e4ab1a0b2431b1ff37482bf
lupuuss <damian.baczynski@gmail.com>
2023-02-20 22:21:57 +0100

    Rename RawResponse generic type to Dto

--- redukt-data-ktor/src/commonMain/kotlin/com/daftmobile/redukt/data/ktor/HttpEndpoint.kt
+++ redukt-data-ktor/src/commonMain/kotlin/com/daftmobile/redukt/data/ktor/HttpEndpoint.kt
@@ -4,40 +4,40 @@ import io.ktor.client.call.body
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.statement.HttpResponse
 
-public interface HttpEndpoint<in Request, RawResponse, out Response> {
+public interface HttpEndpoint<in Request, Dto, out Response> {
 
     public val requestCreator: HttpRequestCreator<Request>
 
-    public val responseReader: HttpResponseReader<RawResponse>
+    public val responseReader: HttpResponseReader<Dto>
 
-    public val responseMapper: HttpResponseMapper<Request, RawResponse, Response>
+    public val responseMapper: HttpResponseMapper<Request, Dto, Response>
 
     public val errorMapper: HttpErrorMapper?
 }
 
 public typealias HttpRequestCreator<Request> = HttpRequestBuilder.(request: Request) -> Unit
 public typealias HttpResponseReader<Response> = suspend HttpResponse.() -> Response
-public typealias HttpResponseMapper<Request, RawResponse, Response> =
-            (request: Request, rawResponse: RawResponse) -> Response
+public typealias HttpResponseMapper<Request, Dto, Response> =
+            (request: Request, dto: Dto) -> Response
 public typealias HttpErrorMapper = (error: Throwable) -> Throwable
 
-public fun <Request, RawResponse, Response> HttpEndpoint(
+public fun <Request, Dto, Response> HttpEndpoint(
     requestCreator: HttpRequestCreator<Request>,
-    responseReader: HttpResponseReader<RawResponse>,
-    responseMapper: HttpResponseMapper<Request, RawResponse, Response>,
+    responseReader: HttpResponseReader<Dto>,
+    responseMapper: HttpResponseMapper<Request, Dto, Response>,
     errorMapper: HttpErrorMapper? = null
-): HttpEndpoint<Request, RawResponse, Response> = HttpEndpointDefinition(
+): HttpEndpoint<Request, Dto, Response> = HttpEndpointDefinition(
     requestCreator = requestCreator,
     responseReader = responseReader,
     responseMapper = responseMapper,
     errorMapper = errorMapper
 )
 
-public inline fun <Request, reified RawResponse, Response> HttpEndpoint(
+public inline fun <Request, reified Dto, Response> HttpEndpoint(
     noinline requestCreator: HttpRequestCreator<Request>,
-    noinline responseMapper: HttpResponseMapper<Request, RawResponse, Response>,
+    noinline responseMapper: HttpResponseMapper<Request, Dto, Response>,
     noinline errorMapper: HttpErrorMapper? = null
-): HttpEndpoint<Request, RawResponse, Response> {
+): HttpEndpoint<Request, Dto, Response> {
     return HttpEndpoint(
         requestCreator = requestCreator,
         responseReader = HttpResponse::body,
@@ -55,9 +55,9 @@ public inline fun <Request, reified Response> HttpEndpoint(
     errorMapper = errorMapper
 )
 
-internal data class HttpEndpointDefinition<in Request, RawResponse, out Response>(
+internal data class HttpEndpointDefinition<in Request, Dto, out Response>(
     override val requestCreator: HttpRequestCreator<Request>,
-    override val responseReader: HttpResponseReader<RawResponse>,
-    override val responseMapper: HttpResponseMapper<Request, RawResponse, Response>,
+    override val responseReader: HttpResponseReader<Dto>,
+    override val responseMapper: HttpResponseMapper<Request, Dto, Response>,
     override val errorMapper: HttpErrorMapper?
-) : HttpEndpoint<Request, RawResponse, Response>
+) : HttpEndpoint<Request, Dto, Response>
