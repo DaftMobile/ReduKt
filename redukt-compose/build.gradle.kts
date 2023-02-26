ed909eb1d495daeccff810d9791ed25b55cf567a
lupuuss <damian.baczynski@gmail.com>
2023-02-26 01:50:49 +0100

    Bump to kotlin 1.8.10 to fix failing tests

--- redukt-compose/build.gradle.kts
+++ redukt-compose/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("org.jetbrains.compose")
 }
 
+compose {
+    kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:1.4.3")
+}
+
 dependencies {
     commonMainApi(ReduKt.core)
     commonMainApi(libs.compose)
