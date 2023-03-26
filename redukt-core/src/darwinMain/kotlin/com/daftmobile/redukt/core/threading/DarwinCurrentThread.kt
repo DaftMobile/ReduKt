123c3530313850ced119deb95c215a90791a94c1
lupuuss <damian.baczynski@gmail.com>
2023-03-26 19:40:04 +0200

    Improve DarwinCurrentThread.kt

--- redukt-core/src/darwinMain/kotlin/com/daftmobile/redukt/core/threading/DarwinCurrentThread.kt
+++ redukt-core/src/darwinMain/kotlin/com/daftmobile/redukt/core/threading/DarwinCurrentThread.kt
@@ -5,4 +5,14 @@ import platform.Foundation.NSThread
 /**
  * Returns [KtThread] depending on [NSThread.currentThread](https://developer.apple.com/documentation/foundation/nsthread/1410679-currentthread) from Apple Foundation Framework.
  */
-public actual fun KtThread.Companion.current(): KtThread = KtThread(NSThread.currentThread.name)
+public actual fun KtThread.Companion.current(): KtThread = NSThread
+    .currentThread()
+    .let(NSThread::identifier)
+    .let(::KtThread)
+
+private val NSThread.identifier: String?
+    get() = when {
+        isMainThread -> "main"
+        !name.isNullOrBlank() -> name
+        else -> description?.takeIf { it.isNotBlank() }
+    }
